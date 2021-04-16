f6672bc8c1a705e04c647653f63746932d093a0c
yurylook <solexy@mail.ru>
2021-04-16 13:49:15 +0300

    Preparation on coffee

--- main.js
+++ main.js
@@ -2,25 +2,55 @@
 
 const COFFEE_ITEMS = document.body.querySelectorAll(".container .coffee-item");
 const COFFEE_BALANCE_DISPLAY = document.body.querySelector(".coffee-balance");
+const PROGRESS = document.body.querySelector(".progress");
+const COFFEE_CUP = document.body.querySelector(".coffee-cup img");
+//const COFFEE_CUP_IMG = COFFEE_CUP.querySelector(".coffee-cup img");
+let progressBar = document.body.querySelector(".progress-bar");
 let moneyBalance = document.body.querySelector(".coffee-balance input");
 let displayText = document.body.querySelector(".display-text");
 
-moneyBalance.value = "300";
+
 for(let coffeeItem of COFFEE_ITEMS) {
     coffeeItem.onclick = function() {
         let activeBalance = +moneyBalance.value - (+this.getAttribute("cost"));
         let coffeeName = this.getAttribute("name");
-        getCurrentBalance(activeBalance, coffeeName);
+        let src = this.querySelector("img").src;
+        console.log (src);
+        getCoffee(activeBalance, coffeeName, src);
     }
 }
 
-const getCurrentBalance = function(activeBalance, coffeeName) {
+const getCoffee = function(activeBalance, coffeeName, src) {
+    let counter = 0;
     if(activeBalance >= 0) {
         moneyBalance.value =  activeBalance;
+        PROGRESS.classList.remove("d-none");
         displayText.innerHTML = "Ваш " + coffeeName + " готовится.";
-        COFFEE_BALANCE_DISPLAY.style.border = "solid 2px green ";
+        moneyBalance.style.background = "";
+        let counterInterval = setInterval(function() {
+            if (counter >= 100 ) {
+                clearInterval( counterInterval);
+                displayText.innerHTML = "Ваш " + coffeeName + " готов.";
+                COFFEE_CUP.classList.remove ("d-none");
+                COFFEE_CUP.src = src;
+                getDisplaySetting ();
+            } else {
+                counter++;
+                progressBar.style.width = counter +"%";
+            }
+        }, 100)
+
     } else {
         displayText.innerHTML = "Недостаточно средств";
-        COFFEE_BALANCE_DISPLAY.style.border = "solid 2px red";
+        moneyBalance.style.background = "red";
+    }
+}
+
+const getDisplaySetting = function() {
+    COFFEE_CUP.onclick = () => {
+        COFFEE_CUP.classList.add ("d-none");
+        displayText.innerHTML = "Выберите кофе";
+        PROGRESS.classList.add("d-none");
+        progressBar.style.width = "0%";
     }
 }

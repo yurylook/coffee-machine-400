6707f848ddb635861d42145d5ca844c930fb8f41
yurylook <solexy@mail.ru>
2021-04-16 14:23:10 +0300

    Preparation on coffee

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const COFFEE_ITEMS = document.body.querySelectorAll(".container .coffee-item");
 const COFFEE_BALANCE_DISPLAY = document.body.querySelector(".coffee-balance");
 const PROGRESS = document.body.querySelector(".progress");
 const COFFEE_CUP = document.body.querySelector(".coffee-cup img");
-//const COFFEE_CUP_IMG = COFFEE_CUP.querySelector(".coffee-cup img");
 let progressBar = document.body.querySelector(".progress-bar");
 let moneyBalance = document.body.querySelector(".coffee-balance input");
 let displayText = document.body.querySelector(".display-text");
+let coffeePreparation = 0;
 
 
 for(let coffeeItem of COFFEE_ITEMS) {
@@ -15,7 +15,7 @@ for(let coffeeItem of COFFEE_ITEMS) {
         let activeBalance = +moneyBalance.value - (+this.getAttribute("cost"));
         let coffeeName = this.getAttribute("name");
         let src = this.querySelector("img").src;
-        console.log (src);
+        if (!coffeePreparation)
         getCoffee(activeBalance, coffeeName, src);
     }
 }
@@ -23,6 +23,7 @@ for(let coffeeItem of COFFEE_ITEMS) {
 const getCoffee = function(activeBalance, coffeeName, src) {
     let counter = 0;
     if(activeBalance >= 0) {
+        coffeePreparation = 1;
         moneyBalance.value =  activeBalance;
         PROGRESS.classList.remove("d-none");
         displayText.innerHTML = "Ваш " + coffeeName + " готовится.";
@@ -52,5 +53,6 @@ const getDisplaySetting = function() {
         displayText.innerHTML = "Выберите кофе";
         PROGRESS.classList.add("d-none");
         progressBar.style.width = "0%";
+        coffeePreparation = 0;
     }
 }
